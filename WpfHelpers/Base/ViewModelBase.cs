3f783e9940d7025c9f96843b76166f19294afcf6
Rubal Walia <walia.rubal@gmail.com>
2018-04-06 01:07:31 +0530

    Added method to get an instance of View from inside of ViewModel.

--- WpfHelpers/Base/ViewModelBase.cs
+++ WpfHelpers/Base/ViewModelBase.cs
@@ -1,7 +1,27 @@
-﻿namespace NullVoidCreations.WpfHelpers.Base
+﻿using System.Reflection;
+using System.Windows.Controls;
+
+namespace NullVoidCreations.WpfHelpers.Base
 {
     public abstract class ViewModelBase: NotificationBase
     {
-        
+        public Control GetView()
+        {
+            var viewModelName = GetType().Name;
+            var viewName = viewModelName.Replace("ViewModel", "View");
+
+            var assembly = Assembly.GetCallingAssembly();
+            foreach(var type in assembly.GetTypes())
+            {
+                if (type.IsClass && type.Name.Equals(viewName))
+                {
+                    var view = assembly.CreateInstance(type.FullName) as Control;
+                    view.DataContext = this;
+                    return view;
+                }
+            }
+
+            return null;
+        }
     }
 }
