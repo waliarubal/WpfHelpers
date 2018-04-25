1c71f56f05fa9e48154fab9b12309547daa2fbec
Rubal Walia <walia.rubal@gmail.com>
2018-04-25 14:09:19 +0530

    Added multi value converter.

---
+++ WpfHelpers/Converters/MultiValueConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace NullVoidCreations.WpfHelpers.Converters
+{
+    public class MultiValueConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            return values;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
