184d06c1ec62bcd427fe3f5ebf07f3d45cbcae52
Rubal Walia <walia.rubal@gmail.com>
2018-05-12 18:01:04 +0530

    Added password box behavior.

---
+++ WpfHelpers/Behaviors/PasswordBoxBindingBehavior.cs
@@ -0,0 +1,41 @@
+﻿using System.Reflection;
+using System.Security;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Interactivity;
+
+namespace NullVoidCreations.WpfHelpers.Behaviors
+{
+    // taken from http://briannoyesblog.azurewebsites.net/2015/03/04/wpf-using-passwordbox-in-mvvm/
+    public class PasswordBoxBindingBehavior : Behavior<PasswordBox>
+    {
+        public static readonly DependencyProperty PasswordProperty = DependencyProperty.Register("Password", typeof(SecureString), typeof(PasswordBoxBindingBehavior), new PropertyMetadata(null));
+
+        #region properties
+
+        public SecureString Password
+        {
+            get { return (SecureString)GetValue(PasswordProperty); }
+            set { SetValue(PasswordProperty, value); }
+        }
+
+        #endregion
+
+        protected override void OnAttached()
+        {
+            AssociatedObject.PasswordChanged += OnPasswordBoxValueChanged;
+        }
+
+        void OnPasswordBoxValueChanged(object sender, RoutedEventArgs e)
+        {
+            var binding = BindingOperations.GetBindingExpression(this, PasswordProperty);
+            if (binding != null)
+            {
+                PropertyInfo property = binding.DataItem.GetType().GetProperty(binding.ParentBinding.Path.Path);
+                if (property != null)
+                    property.SetValue(binding.DataItem, AssociatedObject.SecurePassword, null);
+            }
+        }
+    }
+}
