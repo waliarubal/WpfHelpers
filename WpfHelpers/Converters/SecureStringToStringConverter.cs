760fc56659404fe9471ec5f3bdf33ce1f505aac1
Rubal Walia <walia.rubal@gmail.com>
2018-05-12 19:02:38 +0530

    Added secure string converter.

---
+++ WpfHelpers/Converters/SecureStringToStringConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using System.Security;
+using System.Windows.Data;
+
+namespace NullVoidCreations.WpfHelpers.Converters
+{
+    public class SecureStringToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var secureString = value as SecureString;
+            return secureString == null ? string.Empty : secureString.ToUnsecureString();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var unsecureString = value as string;
+            if (string.IsNullOrEmpty(unsecureString))
+                unsecureString = string.Empty;
+
+            return unsecureString.ToSecureString();
+        }
+    }
+}
