e3696159046f48b835b50cf4564a0c56e72dbda5
Rubal Walia <walia.rubal@gmail.com>
2018-05-28 21:00:42 +0530

    Added methods to show a view in window.

--- WpfHelpers/Base/ViewModelBase.cs
+++ WpfHelpers/Base/ViewModelBase.cs
@@ -1,10 +1,16 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
+using System.Windows;
 using System.Windows.Controls;
 
 namespace NullVoidCreations.WpfHelpers.Base
 {
     public abstract class ViewModelBase: NotificationBase
     {
+        bool _isDialog;
+        object _result;
+        Window _container;
+
         public Control GetView()
         {
             var viewModelName = GetType().Name;
@@ -22,6 +28,37 @@ namespace NullVoidCreations.WpfHelpers.Base
             }
 
             return null;
+        }
+
+        public TResult ShowDialog<TResult>(Window owner = null)
+        {
+            var view = GetView();
+
+            _isDialog = !_isDialog;
+            _container = new Window();
+            if (owner != null)
+            {
+                _container.ShowInTaskbar = false;
+                _container.Owner = owner;
+                _container.WindowStartupLocation = WindowStartupLocation.CenterOwner;
+            }
+            _container.SizeToContent = SizeToContent.Height;
+            _container.WindowStyle = WindowStyle.ToolWindow;
+            _container.Content = view;
+            _container.ShowDialog();
+
+            return (TResult)_result;
+        }
+
+        protected void Close<TResult>(TResult result)
+        {
+            if (_isDialog)
+                throw new InvalidOperationException("Operation is only allowed when open using ShowDialog.");
+
+            _result = result;
+            _container.Close();
+            _container = null;
+            _isDialog = !_isDialog;
         }
     }
 }
