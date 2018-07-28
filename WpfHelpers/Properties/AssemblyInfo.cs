daa0b6492cfcaeb5c9969aadcdc72116ccd2e8bf
Rubal Walia <rubal.walia@dotsquares.com>
2018-07-28 14:43:05 +0530

    Added XML namespace mapping.

--- WpfHelpers/Properties/AssemblyInfo.cs
+++ WpfHelpers/Properties/AssemblyInfo.cs
@@ -1,6 +1,7 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Windows;
+using System.Windows.Markup;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
@@ -13,6 +14,11 @@ using System.Windows;
 [assembly: AssemblyCopyright("Copyright © Rubal Walia 2017 - 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
+
+// namespace mapping
+[assembly:XmlnsPrefix("http://schemas.nullvoidcreations.com/wpfhelpers", "wh")]
+[assembly:XmlnsDefinition("http://schemas.nullvoidcreations.com/wpfhelpers", "NullVoidCreations.WpfHelpers.Behaviors")]
+[assembly: XmlnsDefinition("http://schemas.nullvoidcreations.com/wpfhelpers", "NullVoidCreations.WpfHelpers.Converters")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -49,5 +55,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("18.7.6.12")]
-[assembly: AssemblyFileVersion("18.7.6.12")]
+[assembly: AssemblyVersion("18.7.28.14")]
+[assembly: AssemblyFileVersion("18.7.28.14")]
