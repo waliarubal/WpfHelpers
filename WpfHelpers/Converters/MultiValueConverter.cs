70021db2841a1083f984389de159284154148206
Rubal Walia <rubal.walia@dotsquares.com>
2018-10-03 17:47:25 +0530

    Fixed multi binding converter to retun list.

--- WpfHelpers/Converters/MultiValueConverter.cs
+++ WpfHelpers/Converters/MultiValueConverter.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Globalization;
 using System.Windows.Data;
 
@@ -8,7 +9,10 @@ namespace NullVoidCreations.WpfHelpers.Converters
     {
         public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
         {
-            return values;
+            if (values == null)
+                return null;
+
+            return new List<object>(values);
         }
 
         public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
